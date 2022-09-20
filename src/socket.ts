23f17a10bd689b61496b1ac503c67f9951d6ca04
jstreich13 <jstreich13@gmail.com>
2022-09-20 13:02:49 -0400

    Just fixed a lingering TS issue regarding types on the Home Component. Now working properly with one minor bug. Will come back shortly to patch  and finish styling.

--- src/socket.ts
+++ src/socket.ts
@@ -6,37 +6,68 @@ const EVENTS = {
   connection: "connection",
   CLIENT: {
     CREATE_ROOM: "CREATE_ROOM",
+    SEND_ROOM_MESSAGE: "SEND_ROOM_MESSAGE",
+    JOIN_ROOM: "JOIN_ROOM",
   },
   SERVER: {
     ROOMS: "ROOMS",
     JOINED_ROOM: "JOINED_ROOM",
+    ROOM_MESSAGE: "ROOM_MESSAGE",
   },
 };
 
 const rooms: Record<string, { name: string }> = {};
 
 function socket({ io }: { io: Server }) {
-  logger.info(`Sockets are running`);
+  logger.info(`Sockets enabled`);
 
   io.on(EVENTS.connection, (socket: Socket) => {
-    logger.info(`User synched ${socket.id}`);
+    logger.info(`User connected ${socket.id}`);
+
+    socket.emit(EVENTS.SERVER.ROOMS, rooms);
+
+    // * When a user creates a new room
 
     socket.on(EVENTS.CLIENT.CREATE_ROOM, ({ roomName }) => {
       console.log({ roomName });
-      // Creating a new roomId
+      // creating roomId
       const roomId = nanoid();
-      // Adding the newly created room to Object
-
+      // adding new room to the rooms object
       rooms[roomId] = {
         name: roomName,
       };
 
       socket.join(roomId);
-      // displaying to everyone that there's a new room
+
+      // broadcasting an event that there is a new room
       socket.broadcast.emit(EVENTS.SERVER.ROOMS, rooms);
-      // emit even to room creator of other rooms
+
+      // emitting all potential rooms back to room creator
       socket.emit(EVENTS.SERVER.ROOMS, rooms);
-      // emit event to room creator that he/she joined the room
+      // emitting to user that he/she has joined room
+      socket.emit(EVENTS.SERVER.JOINED_ROOM, roomId);
+    });
+
+    // * When user sends a message in room
+
+    socket.on(
+      EVENTS.CLIENT.SEND_ROOM_MESSAGE,
+      ({ roomId, message, username }) => {
+        const date = new Date();
+
+        socket.to(roomId).emit(EVENTS.SERVER.ROOM_MESSAGE, {
+          message,
+          username,
+          time: `${date.getHours()}:${date.getMinutes()}`,
+        });
+      }
+    );
+
+    // * When user joins room
+
+    socket.on(EVENTS.CLIENT.JOIN_ROOM, (roomId) => {
+      socket.join(roomId);
+
       socket.emit(EVENTS.SERVER.JOINED_ROOM, roomId);
     });
   });
